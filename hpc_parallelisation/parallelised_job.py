c73f7c2eb0c98a13db2abccdf51fce798b37bd85
Martina Oliver Huidobro <martinaoliver1@gmail.com>
2024-03-07 16:55:21 +0000

    parallelised job fixed

--- hpc_parallelisation/parallelised_job.py
+++ hpc_parallelisation/parallelised_job.py
@@ -11,6 +11,10 @@
     To call this script, we use the parallelised_job.pbs script which calls:
     python parallelised_job.py Number_of_Threads
     where Number_of_Threads is an integer argument to the parallelised_job.py function.
+
+    In this script we parallelise a computation which in this case is a simple sum of two numbers.
+    We have a dataframe with many rows and want to sum the values of the two columns in parallel.
+    This operation is not very computationally expensive, but imagine the function was more expensive and it had to be split in batches. 
 ====================================================
 '''
 
@@ -41,8 +45,8 @@ print('Number of Threads set to ', Number_of_Threads)
 
 # Define function to run in parallel
 def my_function(df_batch, path, filename, batch_count):
-    df_batch["sum a+b"] = df_batch["a"] + df_batch["b"]
-    with open(path + f'{filename}{batch_count}.pkl', 'wb') as f:
+    df_batch["sum a+b"] = df_batch["a"] + df_batch["b"] #sums over all rows
+    with open(path + f'{filename}{batch_count}.pkl', 'wb') as f: #saves the dataframe to a pickle file
         pkl.dump(df_batch, f)
     print(df)
 
@@ -66,12 +70,14 @@ path = ''
 filename = 'output_file_'
 for batch_count, start_batch_index in enumerate(batch_indices):
     print('main' + str(start_batch_index))
-    df_batch = df.iloc[start_batch_index:start_batch_index+batch_size]
-    pool_output.append(pool.apply_async(my_function, args=(df_batch, path, filename, batch_count)))
+    df_batch = df.iloc[start_batch_index:start_batch_index+batch_size] #splits the dataframe into batches
+    pool_output.append(pool.apply_async(my_function, args=(df_batch, path, filename, batch_count))) #runs the function in parallel: sends a batch of the dataframe to the function my_function
 
 
 # Close the parallel processing job
 pool.close()
 pool.join()
+
+
 
 
